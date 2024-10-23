667faf97244df9fe71f1ce3555ce62fa32a1b0ff
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2024-10-23 23:47:13 +0530

    lab manual commit 2

--- p1_2.py
+++ p1_2.py
@@ -1,7 +1,5 @@
 # Write a program in Python using function (recursive and non recursive) which will calculate factorial of a number using function. The number is being passed as argument.
 
-x = int(input("Enter number: "))
-product = 1 
 def fact(x):
     pro = 1
     for i in range(1, x+1):
@@ -9,9 +7,15 @@ def fact(x):
     return pro
 def factrec(x):
     product = 1
-    if x == 1:
+    if x <= 1 :
         return 1
     product = product * x * factrec(x - 1)
     return product
-product = factrec(x)
-print("factorial =",product)
+try:
+    x = int(input("Enter number: "))
+except:
+    print("Error occured")
+else:
+    product = 1 
+    product = factrec(x)
+    print("factorial =",product)
